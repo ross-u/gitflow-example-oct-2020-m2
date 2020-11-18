1ad4295473fdf112b00b23a9cbd5325b113932d7
David Espinosa <davesespinosa@gmail.com>
2020-11-18 17:28:02 +0100

    post route added

--- routes/siteRouter.js
+++ routes/siteRouter.js
@@ -3,8 +3,8 @@ const app = require("../app");
 var siteRouter = express.Router();
 
 // Your routes
-siteRouter.get("/profile", (req, res, next) => {
-  res.send("Uros was here");
+siteRouter.post("/profile", (req, res, next) => {
+  res.send("David was here");
 });
 
 module.exports = siteRouter;
