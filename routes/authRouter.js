212983b391d178a4751d99963945dbd5fb489d06
ross-u <cirkovic.uros.srb@gmail.com>
2020-11-18 17:48:15 +0100

    update

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,10 +1,12 @@
 var express = require("express");
+const siteRouter = require("./siteRouter");
 var authRouter = express.Router();
 
 // Your routes
-siteRouter.get('/profile', (req, res, next) => {
-    res.send('Route for people')
-  })
+siteRouter.get("/profile", (req, res, next) => {
+  res.send("Route for people");
+});
+
+siteRouter.get("/logout", (req, res, next) => {});
 
 module.exports = authRouter;
-c
