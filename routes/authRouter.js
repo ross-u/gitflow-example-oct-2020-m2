9a859022ab866b68cfea7a7b5c5046b1c3fa519a
David Espinosa <davesespinosa@gmail.com>
2020-11-18 17:44:06 +0100

    get route added

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -2,5 +2,8 @@ var express = require("express");
 var authRouter = express.Router();
 
 // Your routes
+siteRouter.get('/profile', (req, res, next) => {
+    res.send('Route for people')
+  })
 
 module.exports = authRouter;
